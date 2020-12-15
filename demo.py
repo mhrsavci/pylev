70676c1b69babf33305b3f7e26180475bf99761d
mhrsavci <savcioglu@itu.edu.tr>
2020-12-15 14:12:12 +0300

    Adding demo.py

---
+++ demo.py
@@ -0,0 +1,17 @@
+
+
+import pylev
+
+
+
+while True:
+    string1 = input("Enter String 1: ")
+    string2 = input("Enter String 2: ")
+
+    distance_dl = pylev.damerau_levenshtein(string1,string2)
+
+    print("Damerau Levenshtein Distance ",distance_dl)
+
+    distance_cl = pylev.classic_levenshtein(string1,string2)
+
+    print("Classic Levenshtein Distance ",distance_cl)
